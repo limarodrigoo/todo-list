73a4619fb07ca15168de53bbcfda6043a4851340
Rodrigo Lima <limarodrigoo@outlook.com>
2022-05-20 08:14:41 -0300

    :bento: create Task Form component

---
+++ app/frontend/src/components/TaskForm.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+export default function TaskForm({ onClick }) {
+  const [description, setDescription] = useState('');
+  const [taskState, setTaskState] = useState('');
+
+  const handleclick = () => {
+    onClick(description, taskState);
+  };
+
+  return (
+    <div>
+      {}
+      <form>
+        <input
+          type="text"
+          name="description"
+          placeholder="Digite sua tarefa"
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+        />
+        <input
+          type="text"
+          name="state"
+          placeholder="Digite estado da tarefa"
+          value={taskState}
+          onChange={(event) => setTaskState(event.target.value)}
+        />
+        <button type="button" onClick={handleclick}>Adicionar</button>
+      </form>
+    </div>
+  );
+}
+
+TaskForm.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
